d7bfe196ae0da59eeadd4e0328698fc89ad529b6
Pattanan Piyachotinan <70986870+Petoxic@users.noreply.github.com>
2023-05-14 18:14:36 +0700

    change homepage file location

--- src/Articles/ArticleList.tsx
+++ src/Articles/ArticleList.tsx
@@ -3,35 +3,14 @@ import { styled } from "@mui/material/styles";
 import { AppBar, Link, Typography } from "@mui/material";
 
 import theme from "theme/theme";
+import HomePageBanner from "./HomePageBanner";
 
 const ArticleList: React.FC<{}> = () => {
   return (
     <>
-      <HomePage>
-        <Typography
-          variant="h3"
-          sx={{ color: theme.color.white, fontWeight: "600" }}
-        >
-          conduit
-        </Typography>
-        <Typography
-          variant="h5"
-          sx={{ color: theme.color.white, fontWeight: "200" }}
-        >
-          A place to share your knowledge.
-        </Typography>
-      </HomePage>
+      <HomePageBanner />
     </>
   );
 };
 
 export default ArticleList;
-
-const HomePage = styled("div")`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  background-color: ${theme.color.primary};
-  padding: 32px;
-`;
---
+++ src/Articles/HomePageBanner.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import styled from "@emotion/styled";
+import { Typography } from "@mui/material";
+import theme from "theme/theme";
+
+const HomePageBanner: React.FC<{}> = () => {
+  return (
+    <HomePage>
+      <Typography
+        variant="h3"
+        sx={{ color: theme.color.white, fontWeight: "600" }}
+      >
+        conduit
+      </Typography>
+      <Typography
+        variant="h5"
+        sx={{ color: theme.color.white, fontWeight: "200" }}
+      >
+        A place to share your knowledge.
+      </Typography>
+    </HomePage>
+  );
+};
+
+export default HomePageBanner;
+
+const HomePage = styled("div")`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: ${theme.color.primary};
+  padding: 32px;
+`;
