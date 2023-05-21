b815f94a9a033776aa0114846505ec40db5c2ed5
Pattanan Piyachotinan <70986870+Petoxic@users.noreply.github.com>
2023-05-21 13:41:31 +0700

    add header

--- src/NewArticle/NewArticle.tsx
+++ src/NewArticle/NewArticle.tsx
@@ -1,35 +1,46 @@
 import React from "react";
 import styled from "@emotion/styled";
-import { Button, OutlinedInput } from "@mui/material";
+import { Button, OutlinedInput, Typography } from "@mui/material";
 
 import theme from "theme/theme";
 
 const NewArticle: React.FC<{}> = () => {
   return (
     // TODO: submit data
-    <StyledForm onSubmit={() => console.log("submit!")}>
-      <InputContainer>
-        <StyledInput placeholder="Article Title" />
-        <StyledInput placeholder="What's this article about?" />
-        <StyledInput
-          placeholder="Write your article (in markdown)"
-          multiline
-          rows={6}
-        />
-        {/* TODO: add tags */}
-        <StyledButton
-          type="submit"
-          variant="contained"
-          sx={{ alignSelf: "flex-end" }}
-        >
-          Publish Article
-        </StyledButton>
-      </InputContainer>
-    </StyledForm>
+    <ContentContainer>
+      <Typography variant="h4">Add New Article</Typography>
+      <StyledForm onSubmit={() => console.log("submit!")}>
+        <InputContainer>
+          <StyledInput placeholder="Article Title" />
+          <StyledInput placeholder="What's this article about?" />
+          <StyledInput
+            placeholder="Write your article (in markdown)"
+            multiline
+            rows={6}
+          />
+          {/* TODO: add tags */}
+          <StyledButton
+            type="submit"
+            variant="contained"
+            sx={{ alignSelf: "flex-end" }}
+          >
+            Publish Article
+          </StyledButton>
+        </InputContainer>
+      </StyledForm>
+    </ContentContainer>
   );
 };
 
 export default NewArticle;
+
+const ContentContainer = styled("div")`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+  margin-top: 30px;
+`;
 
 const StyledForm = styled("form")`
   width: 100%;
@@ -46,7 +57,6 @@ const InputContainer = styled("div")`
   flex-direction: column;
   align-items: center;
   gap: 10px;
-  margin-top: 5%;
 `;
 
 const StyledInput = styled(OutlinedInput)`
